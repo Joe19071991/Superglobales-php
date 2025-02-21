2255e5b79df856585ae7920e963eefb26c409d3a
Joe19071991 <lauranove66@gmail.com>
2025-02-21 11:02:52 +0100

    etape 2 ok

--- exercice.php
+++ exercice.php
@@ -1,7 +1,28 @@
 <?php
-if ($_GET['first_name'] == null){
-  echo 'Bonjour anonyme !';
-} else {
-  echo 'Bonjour'.' '.htmlspecialchars($_GET['first_name'].' '.'!');
+if (isset($_GET['first_name'])){
+  $first_name = htmlspecialchars($_GET['first_name']);
+} elseif (isset($_POST['first_name'])){
+    $first_name = htmlspecialchars($_POST['first_name']);
+    if (empty($first_name)){
+        echo 'Bonjour Anonyme';
+    } else {
+        echo 'Bonjour ' . htmlspecialchars($first_name);
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Superglobales</title>
+</head>
+<body>
+<form action="exercice.php" method="post">
+ <p>Votre nom : <input type="text" name="first_name" /></p>
+ <p><input type="submit" name="OK" value="OK"></p>
+</form>
+
+</body>
+</html>
